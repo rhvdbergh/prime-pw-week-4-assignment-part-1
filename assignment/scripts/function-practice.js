64eadce0bcb37fe559697aea3c4a43e7e7d2ff2f
Ronald van der Bergh <rhvdbergh@gmail.com>
2021-09-07 13:39:21 -0500

    q8 functional

--- assignment/scripts/function-practice.js
+++ assignment/scripts/function-practice.js
@@ -81,7 +81,11 @@ console.log('Test - should return false for value: "cat", array: ["snake", "gira
 // 8. Function to check if a letter is the first letter in a
 //    string. Return true if it is, and false otherwise
 function isFirstLetter(letter, string) {
-
+  if (letter === string[0]) {
+    return true;
+  } else {
+    return false;
+  }
 }
 console.log('isFirstLetter - should say true', isFirstLetter('a', 'apple'));
 console.log('isFirstLetter - should say false', isFirstLetter('z', 'apple'));
